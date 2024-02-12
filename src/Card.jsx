b4fc447227d2b182363b832cccb6e999ac54bba6
Samuel <samuelrivba@gmail.com>
2024-02-12 16:12:21 -0500

    Add styles

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,7 +1,6 @@
 const Card = ({ id, name, email }) => {
   return (
     <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <h1>RoboFriends</h1>
       <img src={`https://robohash.org/${id}?200x200`} alt="robots" />
       <div>
         <h2>{name}</h2>
