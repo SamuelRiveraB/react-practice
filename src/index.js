aeada75dfd0539fe05a04c58702f73bd62100cc4
Samuel <samuelrivba@gmail.com>
2024-02-12 15:24:29 -0500

    Map all Card elements

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Card from "./Card";
+import CardList from "./CardList";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
 import { robots } from "./robots";
@@ -9,9 +9,7 @@ import { robots } from "./robots";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* robots.map(robot => { */}
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-    {/* }) */}
+    <CardList robots={robots} />
   </React.StrictMode>
 );
 
